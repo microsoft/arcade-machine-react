15e41f959cc9d6c1183c7a67a04c86f4d97c63ca
Connor Peet <connor@peet.io>
2018-07-18 19:06:25 -0700

    feat(scrolling): optimize algorithm, make it pluggable
    I noticed the rescroll was implement in a way designed to be agnostic to
    arcade-machine. However this induces a very great cost to performance. Instead,
    I introduce a new component and ArcScrollable HOC. arcade-machine will only
    attempt to rescroll these marked containers. I also noticed that we were running
    the full rescroll algorithm, even if the element being scrolled to was already
    on the screen! Fixing these leads to substantial speed improvements,
    particularly in the deeply-nested case.
    
    Overall this pplies the same treatment to scrolling as has been applied
    elsewhere in the project. The scrolling algorithm is now a dependency-injected
    class. We also try to use the browser's native scrollTo with `smooth` behavior
    if possible, rather than animating scroll ourselves. Overall this leads to a
    5-6x performance improvement in the deeply nested benchmark cases.

---
+++ src/scroll/native-smooth-scrolling.ts
@@ -0,0 +1,39 @@
+import { IScrollingAlgorithm } from './';
+import { IScrollableContainer } from './scroll-registry';
+import { horizontalDelta, verticalDelta } from './util';
+
+/**
+ * NativeSmoothScrollingAlgorithm tries to use the new window.scrollTo API
+ * to smoothly move the target element into the view. You can pass a fallback
+ * algorithm to use on platforms where this methos is not supported.
+ */
+export class NativeSmoothScrollingAlgorithm implements IScrollingAlgorithm {
+  constructor(private readonly fallback: IScrollingAlgorithm | null = null) {}
+
+  public scrollTo(
+    parent: Readonly<IScrollableContainer>,
+    targetElement: HTMLElement,
+    rect: ClientRect,
+  ): void {
+    const horizontal = horizontalDelta(rect);
+    const vertical = verticalDelta(rect);
+
+    try {
+      if (parent.vertical && vertical) {
+        parent.element.scrollTo({ top: parent.element.scrollTop + vertical, behavior: 'smooth' });
+      }
+      if (parent.horizontal && horizontal) {
+        parent.element.scrollTo({
+          behavior: 'smooth',
+          left: parent.element.scrollLeft + horizontal,
+        });
+      }
+    } catch (e) {
+      if (!this.fallback) {
+        throw e;
+      }
+
+      this.fallback.scrollTo(parent, targetElement, rect);
+    }
+  }
+}
---
+++ src/scroll/smooth-scrolling-algorithm.ts
@@ -0,0 +1,78 @@
+import { IScrollingAlgorithm } from './index';
+import { IScrollableContainer } from './scroll-registry';
+import { horizontalDelta, verticalDelta } from './util';
+
+/**
+ * An EasingFunction is passed into the SmoothScrollingAlgorithm. It takes
+ * a bounds and a progress percentage, from 0 to 1, and returns the number
+ * we should be at.
+ */
+export type EasingFunction = (start: number, end: number, progress: number) => number;
+
+/**
+ * A quadratic smooth scrolling algorithm.
+ */
+function quadSmoothing(start: number, end: number, progress: number): number {
+  const diff = end - start;
+  if (progress < 0.5) {
+    return diff * (2 * progress ** 2) + start;
+  } else {
+    const displaced = progress - 1;
+    return diff * (-2 * displaced ** 2 + 1) + start;
+  }
+}
+
+/**
+ * SmoothScrollingAlgorithm is a scrolling algorithm that gradually moves
+ * to the target element using the provided algorithm.
+ */
+export class SmoothScrollingAlgorithm implements IScrollingAlgorithm {
+  constructor(
+    private readonly scrollSpeed: number = 1000,
+    private readonly smoothing: EasingFunction = quadSmoothing,
+  ) {}
+
+  public scrollTo(
+    parent: Readonly<IScrollableContainer>,
+    // tslint:disable-next-line
+    _el: HTMLElement,
+    referenceRect: ClientRect,
+  ): void {
+    // Animation function to transition a scroll on the `parent` from the
+    // `original` value to the `target` value by calling `set.
+    const animate = (delta: number, original: number, setter: (x: number) => void) => {
+      if (delta === 0) {
+        return;
+      }
+      const target = original + delta;
+      const start = performance.now();
+      const duration = (Math.abs(target - original) / this.scrollSpeed) * 1000;
+      const run = (now: number) => {
+        const progress = Math.min((now - start) / duration, 1);
+        setter(this.smoothing(original, target, progress));
+
+        if (progress < 1) {
+          requestAnimationFrame(run);
+        }
+      };
+
+      requestAnimationFrame(run);
+    };
+
+    if (parent.horizontal) {
+      animate(
+        horizontalDelta(referenceRect),
+        parent.element.scrollLeft,
+        x => (parent.element.scrollLeft = x),
+      );
+    }
+
+    if (parent.vertical) {
+      animate(
+        verticalDelta(referenceRect),
+        parent.element.scrollTop,
+        x => (parent.element.scrollTop = x),
+      );
+    }
+  }
+}
